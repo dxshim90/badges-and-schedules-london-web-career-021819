1f5ecc012820c05429a292da2fe83c34f0795702
Daniel Shimield <dxshim90@gmail.com>
2019-01-29 19:22:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,6 @@ end
 
 def assign_rooms(array)
   rooms = []
-  array.each_with_index { |name, room| rooms.push("Hello, #{name} 
-  You'll be assigned to #{room}+1}")}
+  array.each_with_index { |name, room| rooms.push("Hello, #{name} You'll be assigned to #{room+1}}")}
   return rooms
 end
